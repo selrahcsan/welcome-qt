b40c7f821eaadfbb0dbe6c2813b3f4ec2a3150ac
selrahcsan <charlesstna@proton.me>
2024-03-08 20:07:53 -0300

    Bug fixes

--- welcome.src/office/office365.sh
+++ welcome.src/office/office365.sh
@@ -1,22 +1,33 @@
 #!/usr/bin/env bash
 
-function instalarPacote(){
-  export DEBIAN_FRONTEND="noninteractive"
 
-  apt update -y
-  wget https://github.com/agam778/MS-365-Electron/releases/download/v1.1.0/MS-365-Electron-v1.1.0-linux-amd64.deb | yad --no-buttons --progress --width=400 --height=100  --borders=32 --fixed --center
-  apt install ./MS-365-Electron-v1.1.0-linux-amd64.deb -y {
-    yad --info --modal --no-cancel --width=400 --height=100 --borders=32 --fixed --center   --button="Ok":0 \
-    --text="O Office 365 foi instalado com sucesso!"
-  } || {
+PACOTE="MS-365-Electron"
+VERSAO="v1.1.0"
+
+ARQUIVO="$PACOTE-$VERSAO-linux-amd64.deb"
+URL="https://github.com/agam778/$PACOTE/releases/download/$VERSAO/$ARQUIVO"
+
+
+export DEBIAN_FRONTEND="noninteractive"
+
+
+instalarPacote(){
+    apt update -y
+    wget "$URL"
+    apt install "./$ARQUIVO" -y && {
+        yad --info --modal --no-cancel --width=400 --height=100 --borders=32 --fixed --center   --button="Ok":0 \
+        --text="O Office 365 foi instalado com sucesso!"
+    } || {
     yad --error --modal --width=400 --height=100  --borders=32 --fixed --center   --button="Ok":0 \
     --text="Não foi possível concluir a instalação...\nPor favor, tente novamente!"
-  }
-  rm MS-365-Electron-v1.1.0-linux-amd64.deb
+    }
 }
 
-instalarPacote | yad --no-buttons --progress --width=400 --height=100  --borders=32 --fixed --center    \
-  --auto-close --pulsate --progress-text= \
---text="\nPor favor, aguarde...\n" --title="Instalando o office 365..."
+rm "$ARQUIVO"
 
+instalarPacote | yad --no-buttons --progress --width=400 --height=100  --borders=32 --fixed --center    \
+    --auto-close --pulsate --progress-text= \
+    --text="\nPor favor, aguarde...\n" --title="Instalando o office 365..."
+
+# Sai do script
 exit
